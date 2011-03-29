ab98cd303698e523e54a5354c403853c7e6ce489
Grant Ammons <grant@pipelinedealsco.com>
2011-03-29 08:33:06 -0400

    bump version

--- lib/screwcap.rb
+++ lib/screwcap.rb
@@ -16,7 +16,7 @@ require 'screwcap/sequence'
 require 'screwcap/task_manager'
 
 module Screwcap
-  VERSION='0.6.3'
+  VERSION='0.7'
 
   class TaskNotFound < RuntimeError; end
   class NoServersDefined < Exception; end
